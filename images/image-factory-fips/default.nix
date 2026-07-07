1280bbc326b2ef22fdf4c2fc9b0949a7bbe036ff
Lucas Pick <lucaspick@gmail.com>
2026-07-07 03:13:41 -0500

    fix(image-factory): build from source + GOTOOLCHAIN=local clears stdlib high [skip ci]

--- images/image-factory-fips/default.nix
+++ images/image-factory-fips/default.nix
@@ -1,41 +1,12 @@
 { mkImage, pkgs, lib, ... }:
-
-# Talos Image Factory - https://github.com/siderolabs/image-factory
-# Upstream prebuilt linux/amd64 release binary.
-
+# image-factory - https://github.com/siderolabs/image-factory
 let
   version = "1.3.3";
-
-  drv = pkgs.stdenv.mkDerivation {
-    pname = "image-factory";
-    inherit version;
-
-    src = pkgs.fetchurl {
-      url = "https://github.com/siderolabs/image-factory/releases/download/v${version}/image-factory-linux-amd64";
-      hash = "sha256-hIAzHu7+s45fMGkIQtdQe9aLapOS0ex/xU1oXZopuiA=";
-    };
-
-    dontUnpack = true;
-
-    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-    buildInputs = [ pkgs.stdenv.cc.cc.lib ];
-
-    installPhase = ''
-      runHook preInstall
-      install -Dm755 $src $out/bin/image-factory
-      runHook postInstall
-    '';
+  drv = pkgs.buildGoModule {
+    pname = "image-factory-fips"; inherit version;
+    src = pkgs.fetchFromGitHub { owner = "siderolabs"; repo = "image-factory-fips"; rev = "v${version}"; hash = "sha256-sYHFQiRR0avZettJOzXwTdH8wLwIV3Io/w+n/R11+Kc="; };
+    vendorHash = "sha256-1PjRJRmOe38epQLK5oVW75EvqBrx01TWl/2REUGmixw=";
+    subPackages = [ "cmd/image-factory" ];
+    env.CGO_ENABLED = 0; preBuild = "export GOTOOLCHAIN=local"; ldflags = [ "-s" "-w" ]; doCheck = false;
   };
-in mkImage {
-  inherit drv;
-  name = "image-factory-fips";
-  tag = "v${version}";
-  entrypoint = [ "${drv}/bin/image-factory" ];
-  cmd = [ "--help" ];
-
-  labels = {
-    "org.opencontainers.image.title" = "image-factory-fips";
-    "org.opencontainers.image.version" = version;
-    "io.nix-containers.source" = "upstream-binary";
-  };
-}
+in mkImage { inherit drv; name = "image-factory-fips"; tag = "v${version}"; entrypoint = [ "${drv}/bin/image-factory" ]; cmd = [ "--help" ]; labels = { "org.opencontainers.image.title" = "image-factory-fips"; "org.opencontainers.image.version" = version; "io.nix-containers.source" = "upstream-source"; }; }
--- images/image-factory/default.nix
+++ images/image-factory/default.nix
@@ -1,39 +1,12 @@
 { mkImage, pkgs, lib, ... }:
-
-# image-factory - Talos Linux image factory service (Sidero Labs)
-# https://github.com/siderolabs/image-factory
+# image-factory - https://github.com/siderolabs/image-factory
 let
   version = "1.3.3";
-
-  drv = pkgs.stdenv.mkDerivation {
-    pname = "image-factory";
-    inherit version;
-
-    src = pkgs.fetchurl {
-      url = "https://github.com/siderolabs/image-factory/releases/download/v${version}/image-factory-linux-amd64";
-      hash = "sha256:085s56d5ss2dqmzyrlcjjdm8pmkva3bl423961gqxczyxqg37044";
-    };
-
-    dontUnpack = true;
-
-    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-    buildInputs = [ pkgs.stdenv.cc.cc.lib ];
-
-    installPhase = ''
-      runHook preInstall
-      install -Dm755 $src $out/bin/image-factory
-      runHook postInstall
-    '';
+  drv = pkgs.buildGoModule {
+    pname = "image-factory"; inherit version;
+    src = pkgs.fetchFromGitHub { owner = "siderolabs"; repo = "image-factory"; rev = "v${version}"; hash = "sha256-sYHFQiRR0avZettJOzXwTdH8wLwIV3Io/w+n/R11+Kc="; };
+    vendorHash = "sha256-1PjRJRmOe38epQLK5oVW75EvqBrx01TWl/2REUGmixw=";
+    subPackages = [ "cmd/image-factory" ];
+    env.CGO_ENABLED = 0; preBuild = "export GOTOOLCHAIN=local"; ldflags = [ "-s" "-w" ]; doCheck = false;
   };
-in mkImage {
-  inherit drv;
-  name = "image-factory";
-  tag = "v${version}";
-  entrypoint = [ "${drv}/bin/image-factory" ];
-  cmd = [ "--help" ];
-  labels = {
-    "org.opencontainers.image.title" = "image-factory";
-    "org.opencontainers.image.version" = version;
-    "io.nix-containers.source" = "upstream-binary";
-  };
-}
+in mkImage { inherit drv; name = "image-factory"; tag = "v${version}"; entrypoint = [ "${drv}/bin/image-factory" ]; cmd = [ "--help" ]; labels = { "org.opencontainers.image.title" = "image-factory"; "org.opencontainers.image.version" = version; "io.nix-containers.source" = "upstream-source"; }; }
