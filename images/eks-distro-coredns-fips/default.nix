01ecfa03880841dd931ace9bf4007b574c068eff
Lucas Pick <lucaspick@gmail.com>
2026-07-07 02:15:00 -0500

    fix(coredns,nats-box families): build from source + GOTOOLCHAIN=local clears stdlib high (-44h) [skip ci]

--- images/eks-distro-coredns-fips/default.nix
+++ images/eks-distro-coredns-fips/default.nix
@@ -1,46 +1,11 @@
 { mkImage, pkgs, lib, ... }:
-
-# EKS Distro CoreDNS (fips variant) - packaged from the upstream CoreDNS release binary.
-# https://github.com/coredns/coredns
+# coredns (eks-distro) - https://github.com/coredns/coredns
 let
   version = "1.14.4";
-
-  drv = pkgs.stdenv.mkDerivation {
-    pname = "eks-distro-coredns-fips";
-    inherit version;
-
-    src = pkgs.fetchurl {
-      url = "https://github.com/coredns/coredns/releases/download/v${version}/coredns_${version}_linux_amd64.tgz";
-      hash = "sha256:0vp3rw2k95gpwdbccm93hnmh7kl8ra1hx9xscpncmgcpidm6l3jv";
-    };
-
-    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-    buildInputs = [ pkgs.stdenv.cc.cc.lib ];
-
-    sourceRoot = ".";
-
-    installPhase = ''
-      runHook preInstall
-      install -Dm755 coredns $out/bin/coredns
-      runHook postInstall
-    '';
-
-    meta = with lib; {
-      description = "CoreDNS DNS server (EKS Distro)";
-      homepage = "https://github.com/coredns/coredns";
-      license = licenses.asl20;
-      platforms = [ "x86_64-linux" ];
-    };
+  drv = pkgs.buildGoModule {
+    pname = "eks-distro-coredns-fips"; inherit version;
+    src = pkgs.fetchFromGitHub { owner = "coredns"; repo = "coredns"; rev = "v${version}"; hash = "sha256-ByfXuFjALQV7iDb+QVGaP1UYDPXZTHAiTdcxNLbETCs="; };
+    vendorHash = "sha256-I+lO6HMtKX8N79Bdm8AzzIZe8mMYjHklZ85HjAjL1lk=";
+    env.CGO_ENABLED = 0; preBuild = "export GOTOOLCHAIN=local"; ldflags = [ "-s" "-w" ]; doCheck = false;
   };
-in mkImage {
-  inherit drv;
-  name = "eks-distro-coredns-fips";
-  tag = "v${version}";
-  entrypoint = [ "${drv}/bin/coredns" ];
-  cmd = [ "--help" ];
-  labels = {
-    "org.opencontainers.image.title" = "eks-distro-coredns-fips";
-    "org.opencontainers.image.version" = version;
-    "io.nix-containers.source" = "upstream-binary";
-  };
-}
+in mkImage { inherit drv; name = "eks-distro-coredns-fips"; tag = "v${version}"; entrypoint = [ "${drv}/bin/coredns" ]; cmd = [ "--help" ]; labels = { "org.opencontainers.image.title" = "eks-distro-coredns-fips"; "org.opencontainers.image.version" = version; "io.nix-containers.source" = "upstream-source"; }; }
--- images/nats-box/default.nix
+++ images/nats-box/default.nix
@@ -1,51 +1,12 @@
 { mkImage, pkgs, lib, ... }:
-
-# nats-box - NATS utility box; its primary tool is the `nats` CLI (natscli)
-# https://github.com/nats-io/natscli
-
+# nats-box (natscli) - https://github.com/nats-io/natscli
 let
   version = "0.4.0";
-
-  drv = pkgs.stdenv.mkDerivation {
-    pname = "natscli";
-    inherit version;
-
-    src = pkgs.fetchurl {
-      url = "https://github.com/nats-io/natscli/releases/download/v${version}/nats-${version}-linux-amd64.zip";
-      hash = "sha256-jb1DfIJrlT29dDLPiQ7yK6PDPczD3OXnGz6NBVQnhJw=";
-    };
-
-    nativeBuildInputs = [ pkgs.autoPatchelfHook pkgs.unzip ];
-    buildInputs = [ pkgs.stdenv.cc.cc.lib ];
-
-    sourceRoot = "nats-${version}-linux-amd64";
-
-    installPhase = ''
-      runHook preInstall
-      install -Dm755 nats $out/bin/nats
-      runHook postInstall
-    '';
-
-    meta = with lib; {
-      description = "NATS command line interface (nats-box)";
-      homepage = "https://github.com/nats-io/natscli";
-      license = licenses.asl20;
-      platforms = [ "x86_64-linux" ];
-    };
+  drv = pkgs.buildGoModule {
+    pname = "nats-box"; inherit version;
+    src = pkgs.fetchFromGitHub { owner = "nats-io"; repo = "natscli"; rev = "v${version}"; hash = "sha256-NF2A4bkGczaH+TYwQnLSvt21uQIk5FZomQuVl22CP30="; };
+    vendorHash = "sha256-mRsHVNs+kyiXP35YQMO9bxWK0sf51hRR7Pcn5oQKl6c=";
+    subPackages = [ "nats" ];
+    env.CGO_ENABLED = 0; preBuild = "export GOTOOLCHAIN=local"; ldflags = [ "-s" "-w" ]; doCheck = false;
   };
-
-in mkImage {
-  inherit drv;
-  name = "nats-box";
-  tag = "v${version}";
-  buildType = "binary";
-  entrypoint = [ "${drv}/bin/nats" ];
-  cmd = [ "--help" ];
-
-  labels = {
-    "org.opencontainers.image.title" = "nats-box";
-    "org.opencontainers.image.description" = "NATS utility box (nats CLI)";
-    "org.opencontainers.image.version" = version;
-    "io.nix-containers.source" = "upstream-binary";
-  };
-}
+in mkImage { inherit drv; name = "nats-box"; tag = "v${version}"; entrypoint = [ "${drv}/bin/nats" ]; cmd = [ "--help" ]; labels = { "org.opencontainers.image.title" = "nats-box"; "org.opencontainers.image.version" = version; "io.nix-containers.source" = "upstream-source"; }; }
