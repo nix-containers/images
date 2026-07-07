6f30c3b13cea6100d2297e5619b1a2c60adfbeb3
Lucas Pick <lucaspick@gmail.com>
2026-07-06 19:14:59 -0500

    fix(helm-set-status,fuse-overlayfs-snapshotter): build from source + GOTOOLCHAIN=local clears stdlib crit [skip ci]

--- images/fuse-overlayfs-snapshotter/default.nix
+++ images/fuse-overlayfs-snapshotter/default.nix
@@ -1,50 +1,24 @@
 { mkImage, pkgs, lib, ... }:
 
-# containerd fuse-overlayfs snapshotter (rootless overlayfs via FUSE)
-# https://github.com/containerd/fuse-overlayfs-snapshotter
-
+# fuse-overlayfs-snapshotter - https://github.com/containerd/fuse-overlayfs-snapshotter
+# Built from source + GOTOOLCHAIN=local to clear stale Go-stdlib CVEs.
 let
   version = "2.1.7";
-
-  drv = pkgs.stdenv.mkDerivation {
+  drv = pkgs.buildGoModule {
     pname = "fuse-overlayfs-snapshotter";
     inherit version;
-
-    src = pkgs.fetchurl {
-      url = "https://github.com/containerd/fuse-overlayfs-snapshotter/releases/download/v${version}/containerd-fuse-overlayfs-${version}-linux-amd64.tar.gz";
-      hash = "sha256:1wvn0f4zp7d65ilfp884ciqnh1p465s1capckkw1lf127h24hhfm";
+    src = pkgs.fetchFromGitHub {
+      owner = "containerd"; repo = "fuse-overlayfs-snapshotter";
+      rev = "v${version}"; hash = "sha256-w9v2OmgTxBmkZDXqXb2zfKb6o60F3X6qGd9+ZXk1lkk=";
     };
-
-    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-    buildInputs = [ pkgs.stdenv.cc.cc.lib ];
-
-    sourceRoot = ".";
-
-    installPhase = ''
-      runHook preInstall
-      install -Dm755 containerd-fuse-overlayfs-grpc $out/bin/containerd-fuse-overlayfs-grpc
-      runHook postInstall
-    '';
-
-    meta = with lib; {
-      description = "containerd snapshotter plugin for fuse-overlayfs";
-      homepage = "https://github.com/containerd/fuse-overlayfs-snapshotter";
-      license = licenses.asl20;
-      platforms = [ "x86_64-linux" ];
-    };
+    vendorHash = "sha256-Ye2JVaf/wx/d4lCjnQmBquSkYkviNVKynDqKweKBtGI=";
+    env.CGO_ENABLED = 0;
+    preBuild = "export GOTOOLCHAIN=local";
+    ldflags = [ "-s" "-w" ];
+    doCheck = false;
   };
-
 in mkImage {
-  inherit drv;
-  name = "fuse-overlayfs-snapshotter";
-  tag = "v${version}";
-  entrypoint = [ "${drv}/bin/containerd-fuse-overlayfs-grpc" ];
-  cmd = [ "--help" ];
-
-  labels = {
-    "org.opencontainers.image.title" = "fuse-overlayfs-snapshotter";
-    "org.opencontainers.image.description" = "containerd snapshotter plugin for fuse-overlayfs";
-    "org.opencontainers.image.version" = version;
-    "io.nix-containers.source" = "upstream-binary";
-  };
+  inherit drv; name = "fuse-overlayfs-snapshotter"; tag = "v${version}";
+  entrypoint = [ "${drv}/bin/containerd-fuse-overlayfs-grpc" ]; cmd = [ "--help" ];
+  labels = { "org.opencontainers.image.title" = "fuse-overlayfs-snapshotter"; "org.opencontainers.image.version" = version; "io.nix-containers.source" = "upstream-source"; };
 }
--- images/helm-set-status/default.nix
+++ images/helm-set-status/default.nix
@@ -1,39 +1,24 @@
 { mkImage, pkgs, lib, ... }:
 
-# helm-set-status - Helm plugin to set the status of a release
-# https://github.com/josegonzalez/helm-set-status
+# helm-set-status - https://github.com/josegonzalez/helm-set-status
+# Built from source + GOTOOLCHAIN=local to clear stale Go-stdlib CVEs.
 let
   version = "0.2.0";
-
-  drv = pkgs.stdenv.mkDerivation {
+  drv = pkgs.buildGoModule {
     pname = "helm-set-status";
     inherit version;
-
-    src = pkgs.fetchurl {
-      url = "https://github.com/josegonzalez/helm-set-status/releases/download/v${version}/helm-set-status_${version}_linux_amd64.tar.gz";
-      hash = "sha256:0pkm8qlhppwylf9384mjjc1ibz333rr7adwm4khqwrqgldgnkc7j";
+    src = pkgs.fetchFromGitHub {
+      owner = "josegonzalez"; repo = "helm-set-status";
+      rev = "v${version}"; hash = "sha256-waUMgQQl/A9v9q9tGI00cI/neQVehKjhS3bGQ/IV/b0=";
     };
-
-    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-    buildInputs = [ pkgs.stdenv.cc.cc.lib ];
-
-    sourceRoot = ".";
-
-    installPhase = ''
-      runHook preInstall
-      install -Dm755 helm-set-status $out/bin/helm-set-status
-      runHook postInstall
-    '';
+    vendorHash = "sha256-VjVv1J9pbsun3/2MDVehVTA3cQCoprDWleReqAAK/Xs=";
+    env.CGO_ENABLED = 0;
+    preBuild = "export GOTOOLCHAIN=local";
+    ldflags = [ "-s" "-w" ];
+    doCheck = false;
   };
 in mkImage {
-  inherit drv;
-  name = "helm-set-status";
-  tag = "v${version}";
-  entrypoint = [ "${drv}/bin/helm-set-status" ];
-  cmd = [ "--help" ];
-  labels = {
-    "org.opencontainers.image.title" = "helm-set-status";
-    "org.opencontainers.image.version" = version;
-    "io.nix-containers.source" = "upstream-binary";
-  };
+  inherit drv; name = "helm-set-status"; tag = "v${version}";
+  entrypoint = [ "${drv}/bin/helm-set-status" ]; cmd = [ "--help" ];
+  labels = { "org.opencontainers.image.title" = "helm-set-status"; "org.opencontainers.image.version" = version; "io.nix-containers.source" = "upstream-source"; };
 }
