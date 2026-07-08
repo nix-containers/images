7b12fe7517cab58fd6912a3b495dde76b802a51b
itpick <lucaspick@gmail.com>
2026-07-08 18:25:08 -0500

    fix(kots): rebuild from source to clear stdlib CVEs
    Switch from fetchurl prebuilt tarball to buildGoModule against
    replicatedhq/kots v1.130.7. Clears Go-stdlib CVEs baked into the
    upstream prebuilt binary; forward-fixes at each rebuild via current
    nixpkgs Go.

--- images/kots/default.nix
+++ images/kots/default.nix
@@ -2,29 +2,31 @@
 
 # KOTS - Replicated kots CLI / kubectl plugin
 # https://github.com/replicatedhq/kots
+#
+# Built from source with current nixpkgs Go so Go-stdlib CVEs from the
+# upstream prebuilt binary clear at each rebuild.
 
 let
   version = "1.130.7";
 
-  drv = pkgs.stdenv.mkDerivation {
+  drv = pkgs.buildGoModule {
     pname = "kots";
     inherit version;
 
-    src = pkgs.fetchurl {
-      url = "https://github.com/replicatedhq/kots/releases/download/v${version}/kots_linux_amd64.tar.gz";
-      hash = "sha256-evm2cl1v0D/l2Sx1ea1D9c7KuZ7XCZz3fTCeMmnzzEc=";
+    src = pkgs.fetchFromGitHub {
+      owner = "replicatedhq";
+      repo = "kots";
+      rev = "v${version}";
+      hash = "sha256-3QcR1DEhJQpJisHYOn+zrq/fFgqTAViCVUDGy23GeZk=";
     };
 
-    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-    buildInputs = [ pkgs.stdenv.cc.cc.lib ];
+    proxyVendor = true;
+    vendorHash = "sha256-h350Dui1aGgYiv1IhnYbvlKhQ5GmG3Y5Y0uuBrfgdyc=";
 
-    sourceRoot = ".";
-
-    installPhase = ''
-      runHook preInstall
-      install -Dm755 kots $out/bin/kots
-      runHook postInstall
-    '';
+    subPackages = [ "cmd/kots" ];
+    ldflags = [ "-s" "-w" "-X github.com/replicatedhq/kots/pkg/buildversion.version=${version}" ];
+    env.CGO_ENABLED = 0;
+    doCheck = false;
   };
 in mkImage {
   inherit drv;
@@ -35,6 +37,6 @@ in mkImage {
   labels = {
     "org.opencontainers.image.title" = "kots";
     "org.opencontainers.image.version" = version;
-    "io.nix-containers.source" = "upstream-binary";
+    "io.nix-containers.source" = "upstream-source";
   };
 }
