4e69e8e849335653155d0ca6359c4a7cef04edc5
itpick <lucaspick@gmail.com>
2026-07-09 10:48:33 -0500

    fix(rke2-runtime): rebuild from source via pkgs.rke2 (#696)
    Switch rke2-runtime from Rancher's prebuilt Linux binary (fetchurl +
    autoPatchelfHook) to pkgs.rke2 (nixpkgs buildGoModule). Same binary,
    but Go stdlib CVE fixes now cascade in at every rebuild.

--- images/rke2-runtime/default.nix
+++ images/rke2-runtime/default.nix
@@ -2,39 +2,13 @@
 
 # rke2-runtime - RKE2 (Rancher Kubernetes Engine 2) runtime binary
 # https://github.com/rancher/rke2
-
+#
+# Uses pkgs.rke2 (built from source via buildGoModule in nixpkgs) instead of
+# the upstream Rancher-published prebuilt binary; picks up current Go stdlib
+# CVE fixes at each rebuild.
 let
-  version = "1.36.2+rke2r1";
-
-  drv = pkgs.stdenv.mkDerivation {
-    pname = "rke2";
-    inherit version;
-
-    src = pkgs.fetchurl {
-      url = "https://github.com/rancher/rke2/releases/download/v1.36.2%2Brke2r1/rke2.linux-amd64";
-      hash = "sha256:0xqjrrcqdpi5yv4kiq6d4ld077nl07825g0cfilvlld1ay3yzjnp";
-    };
-
-    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-
-    buildInputs = [ pkgs.stdenv.cc.cc.lib pkgs.zlib ];
-
-    dontUnpack = true;
-
-    installPhase = ''
-      runHook preInstall
-      install -Dm755 $src $out/bin/rke2
-      runHook postInstall
-    '';
-
-    meta = with lib; {
-      description = "RKE2 (Rancher Kubernetes Engine 2) runtime";
-      homepage = "https://github.com/rancher/rke2";
-      license = licenses.asl20;
-      platforms = [ "x86_64-linux" ];
-    };
-  };
-
+  drv = pkgs.rke2;
+  version = drv.version;
 in mkImage {
   inherit drv;
   name = "rke2-runtime";
@@ -46,6 +20,6 @@ in mkImage {
     "org.opencontainers.image.title" = "rke2-runtime";
     "org.opencontainers.image.description" = "RKE2 Kubernetes distribution runtime";
     "org.opencontainers.image.version" = version;
-    "io.nix-containers.source" = "upstream-binary";
+    "io.nix-containers.source" = "nixpkgs";
   };
 }
