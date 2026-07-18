ab5873c807c256e51367de50f3464e6398c2c87e
itpick <13190002+itpick@users.noreply.github.com>
2026-07-18 07:29:42 +0000

    chore(deps): automated package + flake updates

--- pkgs/argo-rollouts/default.nix
+++ pkgs/argo-rollouts/default.nix
@@ -4,13 +4,13 @@
 
 buildGoModule rec {
   pname = "argo-rollouts";
-  version = "1.9.0";
+  version = "1.9.1";
 
   src = fetchFromGitHub {
     owner = "argoproj";
     repo = "argo-rollouts";
     rev = "v${version}";
-    hash = "sha256-qpTilslCu9rmBVMo73lHnKD8NPxLHSzeBwkWhEB4If4=";
+    hash = "sha256-DQD4tQ3NrmTujHlf23PHW88z0cY3gM5GCkv3s4/D6wg=";
   };
 
   vendorHash = "sha256-bF4jIEEG5DFhtDdy8LwK6SfE5OdyUsDjOIbAddvb5V8=";
