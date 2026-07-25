72a2b732e5e9e19c4d332cd62bb701ce3c1821e1
itpick <13190002+itpick@users.noreply.github.com>
2026-07-25 07:54:35 +0000

    chore(deps): automated package + flake updates

--- pkgs/pulumi/default.nix
+++ pkgs/pulumi/default.nix
@@ -5,11 +5,11 @@
 # Uses the upstream statically-linked prebuilt binary tarball.
 
 let
-  version = "3.253.0";
+  version = "3.254.0";
   srcs = {
     x86_64-linux = fetchurl {
       url = "https://github.com/pulumi/pulumi/releases/download/v${version}/pulumi-v${version}-linux-x64.tar.gz";
-      hash = "sha256-MxYeUhKA5sdzlaRjRNul/5wRipDfJUgB5mIIcUO00aw=";
+      hash = "sha256-shZ8CTsOv8kYRW0HCr3g50CidsPm3AoTaEvHQwY2TCI=";
     };
     aarch64-linux = fetchurl {
       url = "https://github.com/pulumi/pulumi/releases/download/v${version}/pulumi-v${version}-linux-arm64.tar.gz";
