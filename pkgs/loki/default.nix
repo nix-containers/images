36d46a327539763615eb74f55ab0f4f9e824258a
itpick <13190002+itpick@users.noreply.github.com>
2026-08-02 08:08:42 +0000

    chore(deps): automated package + flake updates

--- pkgs/loki/default.nix
+++ pkgs/loki/default.nix
@@ -4,7 +4,7 @@
 
 buildGoModule rec {
   pname = "loki";
-  version = "3.7.4";
+  version = "helm-loki-7.2.0";
 
   src = fetchFromGitHub {
     owner = "grafana";
--- pkgs/mimir/default.nix
+++ pkgs/mimir/default.nix
@@ -4,7 +4,7 @@
 
 buildGoModule rec {
   pname = "mimir";
-  version = "mimir-distributed-6.2.0-weekly.402";
+  version = "distributed-6.2.0-weekly.402";
 
   src = fetchFromGitHub {
     owner = "grafana";
